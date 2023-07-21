993a3539196d303de4df07d4f2355fabd22010be
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-07-22 01:07:37 +0700

    refactor
    activity login and activity registrasi

--- app/src/main/java/com/rifqipadisiliwangi/sismartpju/view/auth/LoginActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/sismartpju/view/auth/LoginActivity.kt
@@ -1,17 +1,60 @@
 package com.rifqipadisiliwangi.sismartpju.view.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import androidx.appcompat.app.AlertDialog
 import com.rifqipadisiliwangi.sismartpju.R
 import com.rifqipadisiliwangi.sismartpju.databinding.ActivityLoginBinding
+import android.content.DialogInterface
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
-
+    private var isPasswordVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.showPassBtn.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            togglePasswordVisibility()
+        }
+
+        binding.txtRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+    }
+    private fun togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            // Show the password
+            binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT
+            binding.showPassBtn.setImageResource(R.drawable.ic_register_pass) // Replace with your hide password icon
+        } else {
+            // Hide the password
+            binding.etPassword.inputType =
+                InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+            binding.showPassBtn.setImageResource(R.drawable.ic_register_pass) // Replace with your show password icon
+        }
+
+        // Move the cursor to the end of the password field to maintain cursor position
+        binding.etPassword.setSelection(binding.etPassword.text.length)
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+
+        AlertDialog.Builder(this)
+            .setTitle("Tutup Aplikasi")
+            .setMessage("Ingin Keluar Aplikasi?")
+            .setPositiveButton("Ya"){ _: DialogInterface, i: Int ->
+                finishAffinity()
+            }
+            .setNegativeButton("Tidak"){ dialogInterface: DialogInterface, i: Int ->
+                dialogInterface.dismiss()
+            }
+            .show()
     }
 }
--- app/src/main/java/com/rifqipadisiliwangi/sismartpju/view/auth/RegisterActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/sismartpju/view/auth/RegisterActivity.kt
@@ -1,17 +1,66 @@
 package com.rifqipadisiliwangi.sismartpju.view.auth
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import androidx.appcompat.app.AlertDialog
 import com.rifqipadisiliwangi.sismartpju.R
 import com.rifqipadisiliwangi.sismartpju.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterBinding
-
+    private var isPasswordVisible = false
+    private var isPasswordVisibleConfirm = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.showPasswordButton.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            togglePasswordVisibility()
+        }
+
+        binding.showConfirmPasswordButton.setOnClickListener {
+            isPasswordVisibleConfirm = !isPasswordVisibleConfirm
+            togglePasswordVisibility()
+        }
+
+        binding.txtLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finishAndRemoveTask()
+        }
+
     }
+    private fun togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            // Show the password
+            binding.etPassword.inputType = InputType.TYPE_CLASS_TEXT
+            binding.showPasswordButton.setImageResource(R.drawable.ic_register_pass) // Replace with your hide password icon
+        } else {
+            // Hide the password
+            binding.etPassword.inputType =
+                InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+            binding.showPasswordButton.setImageResource(R.drawable.ic_register_pass) // Replace with your show password icon
+        }
+
+        if (isPasswordVisibleConfirm) {
+            // Show the password
+            binding.etConfirmPassword.inputType = InputType.TYPE_CLASS_TEXT
+            binding.showConfirmPasswordButton.setImageResource(R.drawable.ic_register_pass) // Replace with your hide password icon
+        } else {
+            // Hide the password
+            binding.etConfirmPassword.inputType =
+                InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+            binding.showConfirmPasswordButton.setImageResource(R.drawable.ic_register_pass) // Replace with your show password icon
+        }
+
+        // Move the cursor to the end of the password field to maintain cursor position
+        binding.etPassword.setSelection(binding.etPassword.text.length)
+        binding.etConfirmPassword.setSelection(binding.etConfirmPassword.text.length)
+    }
+
 }
